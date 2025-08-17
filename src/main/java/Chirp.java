60131c83538c56f4da0505068ddb28b56c7c8374
huajun07 <teowhuajun07@gmail.com>
2025-08-17 17:05:58 +0800

    Make Chirp echo input

--- src/main/java/Chirp.java
+++ src/main/java/Chirp.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Chirp {
     private static final String name = "Chirp";
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         greet();
-        exit();
+        while (true) {
+            String input = prompt();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            } else {
+                echo(input);
+            }
+        }
+    }
+
+    private static String prompt() {
+        System.out.print("> ");
+        String input = sc.nextLine();
+        return input;
     }
 
     private static void printLine() {
@@ -15,14 +32,18 @@ public class Chirp {
         System.out.println(" Hello! I'm " + name);
         System.out.println(" What can I do for you?");
         printLine();
-        System.out.println();
     }
 
     private static void exit() {
         printLine();
         System.out.println(" Bye. Hope to see you again soon!");
         printLine();
-        System.out.println();
+    }
+
+    private static void echo(String str) {
+        printLine();
+        System.out.println(str);
+        printLine();
     }
 
 }
